c70c41894284f7bc6911f552bbe098a04f218665
smarcher1 <investorsmarch@gmail.com>
2022-09-11 14:13:30 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - print a string using puts function
+ *
+ * return: 0
+ */
+int main(void)
+{
+	char put[] = "Programming is like building a multilingual puzzle,";
+
+	puts(put);
+	return (0);
+}
+
