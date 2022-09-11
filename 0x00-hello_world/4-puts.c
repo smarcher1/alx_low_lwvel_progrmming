1d87a3d55e47a717e7f776003dff3f15ac667572
smarcher1 <investorsmarch@gmail.com>
2022-09-11 15:10:45 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,7 +8,7 @@
 int main(void)
 
 {
-	char put[] = "\"Programming is like building a multilingual puzzle,";
+	char put[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(put);
 return (0);
