33f7f84feb1e36698cfe724bf7387e7c9c6c06f3
smarcher1 <investorsmarch@gmail.com>
2022-09-12 14:27:30 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+  * main - computes sizes of various data types
+  *
+  *Return: zero
+  */
+int main(void)
+{
+	char c;
+	int i;
+	float f;
+
+	printf("Size of   char:%lu bytes\n", sizeof(c));
+	printf("Size of int:%lu bytes\n", sizeof(i));
+	printf("Size of  long int:%lu bytes\n", sizeof(long int));
+	printf("Size of float: %lu bytes\n", sizeof(f));
+return (0);
+}
