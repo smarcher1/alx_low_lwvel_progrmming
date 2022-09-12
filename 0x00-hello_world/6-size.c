01726b290060f9f1793b669aa29a9a4c63a33414
smarcher1 <investorsmarch@gmail.com>
2022-09-12 14:35:33 -0700

    added lonf long int size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,6 +13,7 @@ int main(void)
 	printf("Size of   char:%lu bytes\n", sizeof(c));
 	printf("Size of int:%lu bytes\n", sizeof(i));
 	printf("Size of  long int:%lu bytes\n", sizeof(long int));
+	printf("Size of long  long int:%lu bytes\n", sizeof(long long int));
 	printf("Size of float: %lu bytes\n", sizeof(f));
 return (0);
 }
